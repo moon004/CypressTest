6b5841c72f3c60c3a796e1787592e41c400f2082
oo.siang.hwa <oo.siang.hwa@technoMacBook-Pro.local>
2022-04-22 09:27:04 +0900

    Commit amplify and cypress file

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifycypressb0c68ead": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
